d8d4466f374b2d2a9943d85c9713763c84a56fdc
Kseniia <betrezen145@gmail.com>
2023-10-06 14:51:43 +0300

    Update Main.java

--- Task5_04/Main.java
+++ Task5_04/Main.java
@@ -3,16 +3,21 @@ package Task5_04;
 
 public class Main {
     public static void main(String[] args) {
-        Car ford=new Car("Ford", 100, 150, 20000);
-        Car mitsubishi=new Car("Mitsubishi", 140, 170, 29000);
-        Car toyota=new Car("Toyota", 200, 200, 27500);
-        Car kia=new Car("KIA Rio", 200, 189, 30000);
+        Car ford=new Ford("Ford", 30, 450, 100000);
+        Car mitsubishi=new Mitsubishi("Mitsubishi", 140, 170, 29000);
+        Car toyota=new Toyota("Toyota", 200, 200, 27500);
+        Car kia=new KIA("KIA Rio", 220, 189, 30000);
+
+        // перевірка даних на прикладі машини Ford
+        ford.setPrice(ford.checkValuePrice(ford.getPrice()));
+        ford.setPower(ford.checkValuePower(ford.getPower()));
+        ford.setSpeed(ford.checkValueSpeed(ford.getSpeed()));
 
         CarInfo infoList=new CarInfo();
         infoList.addCar(ford);
-        infoList.addCar(ford);
-        infoList.addCar(ford);
-        infoList.addCar(ford);
+        infoList.addCar(mitsubishi);
+        infoList.addCar(toyota);
+        infoList.addCar(kia);
         System.out.println("\nFastest car: "+infoList.fastestCar());
         System.out.println("\nThe most expensive car: "+infoList.expensiveCar());
         System.out.println("\nThe most powerful car: "+infoList.powerCar());
